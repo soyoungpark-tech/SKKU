beb27a5600a3fa1e63243f16f5c0769c148f737c
soyoungpark-tech <ps.young149@gmail.com>
2021-06-23 11:39:51 +0900

    setting

---
+++ js/script.js
@@ -0,0 +1,10 @@
+'user strict'
+
+$(function(){
+    $('.main__banner').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+})
